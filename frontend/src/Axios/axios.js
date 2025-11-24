5ab443847e818ca7812e2043699279eb75ab65bd
sai-rathod <sahilrathodrk31@gmail.com>
2025-11-24 15:41:18 +0000

    final k8s/manifests

--- frontend/src/Axios/axios.js
+++ frontend/src/Axios/axios.js
@@ -1,5 +1,6 @@
 import axios from "axios"
 const instance = axios.create({
-    baseURL:"http://localhost:8000/api"
+    baseURL: "http://172.23.76.115/api"  // Production (Kubernetes)
+    // baseURL: "http://localhost:8000/api"  // Local development
 })
 export default instance
